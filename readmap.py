14b060294eaca42f720eaef6688a6468fe3a2c60
Hyrancood <max57shapiro@gmail.com>
2024-11-24 18:43:21 +0300

    maps validation
    - maps validation added
    - default map edited (minor changes)
    Need:
    - make tests for validator
    - edit map reader (and for map drawing)

--- readmap.py
+++ readmap.py
@@ -7,6 +7,8 @@ def parse_value(value):
 def parse_object(object_string):
     obj = {}
     for pair in object_string.split(","):
+        if pair.count(":") != 1:
+            raise ValueError(f"Invalid syntax: object '{pair}' hasn't (or has >1) separator ':'")
         key, value = pair.split(":")
         obj[key.strip()] = parse_value(value.strip())
     return obj
@@ -19,6 +21,8 @@ def parse_map(file_content):
 
     for line in file_content:
         line = line.strip()
+        if line == "":
+            continue
         if state == "READING":
             if line.startswith("-"):
                 data[current_key].append(parse_object(line[1:].strip()))
@@ -34,6 +38,206 @@ def parse_map(file_content):
     return data
 
 
+def check_type_valid(parameter, value, t):
+    if not isinstance(value, t):
+        raise ValueError(f"Invalid value: '{parameter}' should be integer, not {type(value)}")
+
+
+def is_name_valid(values):
+    if len(values) == 0:
+        raise ValueError("Invalid structure: name section has no objects")
+    parsed = [parse_object(val) for val in values]
+    langs = {
+        "ru": None,
+        "en": None
+    }
+    for obj in parsed:
+        for lang in obj:
+            if lang != "ru" and lang != "en":
+                raise ValueError(f"Invalid syntax: unexpected language: {lang}")
+            if langs[lang] is not None:
+                raise ValueError(f"Invalid syntax: repeated language: {lang}")
+            langs[lang] = obj[lang]
+    return True
+
+
+def is_sizes_valid(values):
+    if len(values) == 0:
+        raise ValueError("Invalid structure: sizes section has no objects")
+    sizes = {
+        'x': None,
+        'y': None
+    }
+    parsed = [parse_object(val) for val in values]
+    for obj in parsed:
+        for size in obj:
+            if size != 'x' and size != 'y':
+                raise ValueError(f"Invalid syntax: unexpected size: '{size}'")
+            if sizes[size] is not None:
+                raise ValueError(f"Invalid syntax: repeated size: '{size}'")
+            if obj[size] == 0:
+                raise ValueError(f"Invalid value: '{size}'-size should be >0")
+            check_type_valid(size, obj[size], int)
+            sizes[size] = obj[size]
+    if not (isinstance(sizes["x"], int) and isinstance(sizes["y"], int)):
+        if isinstance(sizes["x"], int):
+            raise ValueError(f"Invalid structure: sizes should contains 'x' and 'y', has only 'x'")
+        raise ValueError(f"Invalid structure: sizes should contains 'x' and 'y', has only 'y'")
+    if sizes['x'] > 40 or sizes['x'] < 5:
+        raise ValueError(f"Invalid value: 'x'-size should be between 5 and 40, 'x' = {sizes['x']}")
+    if sizes['y'] > 22 or sizes['y'] < 3:
+        raise ValueError(f"Invalid value: 'x'-size should be between 3 and 22, 'y' = {sizes['y']}")
+    return True, sizes['x'], sizes['y']
+
+
+def check_if_more_then_zero(parameter, value):
+    if value < 1:
+        raise ValueError(f"Invalid value: '{parameter}'-parameter should be >0")
+
+
+def check_if_point_in_map(point, map_size):
+    if all(cord > 0 for cord in point):
+        if point[0] <= map_size[0] and point[1] <= map_size[1]:
+            return True
+        else:
+            raise ValueError(f"Invalid value: point {point} not fit in map sizes {map_size}")
+    raise ValueError(f"Invalid value: point {point} should be between (0, 0) and {map_size}")
+
+
+def is_platforms_valid(values, map_x_size, map_y_size, map):
+    parsed = [parse_object(val) for val in values]
+    for platform in parsed:
+        if 'x' not in platform:
+            raise ValueError("Invalid syntax: every object in 'platforms' section should has 'x' parameter")
+        if 'y' not in platform:
+            raise ValueError("Invalid syntax: every object in 'platforms' section should has 'y' parameter")
+        params = (('x', platform['x']), ('y', platform['y']),
+                  ('w', platform.get('w', 1)), ('h', platform.get('h', 1)))
+        for param in params:
+            check_type_valid(*param, int)
+            check_if_more_then_zero(*param)
+        check_if_point_in_map((params[0][1], params[1][1]), (map_x_size, map_y_size))
+        check_if_point_in_map((params[0][1] + params[2][1] - 1, params[1][1]), (map_x_size, map_y_size))
+        check_if_point_in_map((params[0][1], params[1][1] + params[3][1] - 1), (map_x_size, map_y_size))
+        check_if_point_in_map(
+            (params[0][1] + params[2][1] - 1, params[1][1] + params[3][1] - 1),
+            (map_x_size, map_y_size))
+        for x in range(params[0][1]-1, params[0][1]-1+params[2][1]):
+            for y in range(map_y_size - params[1][1] - params[3][1] + 1,
+                           map_y_size - params[1][1] + 1):
+                map[y][x] = True
+    return True
+
+
+def is_thorns_valid(values, map_x_size, map_y_size, map):
+    parsed = [parse_object(val) for val in values]
+    for thorns in parsed:
+        if 'x' not in thorns:
+            raise ValueError("Invalid syntax: every object in 'thorns' section should has 'x' parameter")
+        if 'y' not in thorns:
+            raise ValueError("Invalid syntax: every object in 'thorns' section should has 'y' parameter")
+        x = thorns['x']
+        y = thorns['y']
+        check_type_valid('x', x, int)
+        check_type_valid('y', y, int)
+        check_if_point_in_map((x, y), (map_x_size, map_y_size))
+        if map[map_y_size-y][x-1]:
+            raise ValueError(f"Invalid structure: object 'thorns{(x, y)}' overlaps platform")
+        if map_y_size > map_y_size - y + 1 >= 0:
+            if not map[map_y_size - y + 1][x-1]:
+                raise ValueError(f"Invalid structure: object 'thorns{(x, y)}' float in air, should be placed on platform or on bottom border of map")
+    return True
+
+
+def is_flags_valid(values, map_x_size, map_y_size, map):
+    flags = {
+        "red": False,
+        "blue": False
+    }
+    parsed = [parse_object(val) for val in values]
+    for flag in parsed:
+        if 'x' not in flag:
+            raise ValueError("Invalid syntax: every object in 'flag' section should has 'x' parameter")
+        if 'y' not in flag:
+            raise ValueError("Invalid syntax: every object in 'flag' section should has 'y' parameter")
+        if 'color' not in flag:
+            raise ValueError("Invalid syntax: every object in 'flag' section should has 'color' parameter")
+        x = flag['x']
+        y = flag['y']
+        color = flag["color"]
+        if color not in flags:
+            raise ValueError(f"Invalid value: parameter 'color' for object 'flag' should be 'blue' or 'red', get {color}")
+        if flags[color]:
+            raise ValueError(f"Invalid structure: repeated object 'flag' with 'color'='{color}'")
+        check_type_valid('x', x, int)
+        check_type_valid('y', y, int)
+        check_if_point_in_map((x, y), (map_x_size, map_y_size))
+        if map[map_y_size - y][x-1]:
+            raise ValueError(f"Invalid structure: object 'flag{(x, y)}' overlaps platform")
+        if map_y_size > map_y_size - y + 1 >= 0:
+            if not map[map_y_size - y + 1][x-1]:
+                raise ValueError(f"Invalid structure: object 'flag{(x, y)}' float in air, should be placed on platform or on bottom border of map")
+        flags[color] = True
+    return flags['blue'] and flags['red']
+
+
+def is_valid(file_content):
+    state = "KEY"
+    sections = {}
+    key = None
+    section = []
+    for line in file_content:
+        line = line.strip()
+        if line == "":
+            continue
+        if state == "READING":
+            if line.startswith("-"):
+                section.append(line[1:].strip())
+            else:
+                sections[key] = section.copy()
+                section = []
+                state = "KEY"
+        if state == "KEY":
+            if line.endswith(":"):
+                key = line[:-1]
+                if key not in ("name", "sizes", "platforms", "thorns", "flags"):
+                    raise ValueError(f"Invalid syntax: unexpected key: '{key}'")
+                if key in sections:
+                    raise ValueError(f"Invalid syntax: repeating key: '{key}'")
+                state = "READING"
+            else:
+                raise ValueError("Invalid syntax: expected key ending with ':'")
+    sections[key] = section.copy()
+    
+    if "name" not in sections:
+        raise ValueError("Invalid structure: key 'name' wasn't found")
+    if is_name_valid(sections["name"]):
+        pass
+    if "sizes" not in sections:
+        raise ValueError("Invalid structure: key 'sizes' wasn't found")
+    valid, x, y = is_sizes_valid(sections["sizes"])
+    if valid:
+        map = [[False for __ in range(x)] for _ in range(y)]
+        if "platforms" in sections:
+            valid = is_platforms_valid(sections["platforms"], x, y, map)
+            if valid:
+                pass
+        if "thorns" in sections:
+            if is_thorns_valid(sections['thorns'], x, y, map):
+                pass
+        if "flags" not in sections:
+            raise ValueError("Invalid structure: key 'flags' wasn't found")
+        if is_flags_valid(sections['flags'], x, y, map):
+            pass
+    return True
+
+
 def from_file(path):
     with open(path) as file:
-        return parse_map(file)
+        lines = list(file.readlines())
+        if is_valid(lines):
+            return parse_map(lines)
+
+
+if __name__ == "__main__":
+    print(from_file("maps/default.ctfmap"))

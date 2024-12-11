5d27fe972947049b9630fc647664d9902d0fbf10
yenaii <andrejsuslov1441@gmail.com>
2024-12-11 22:29:13 +0300

    first try

--- abilities.py
+++ abilities.py
@@ -1,6 +1,13 @@
-class Freeze:
-    def activate(self,p1,p2):
-        if ((p1.x-p2.x)**2+(p1.y-p2.y)**2)**0.5<=200:
-            p2.speed/=2
-    def deactivate(self,p2):
+class Abilities:
+    def swap(self,p1,p2):
+        if p1.distance(p2)<=100 and p1.cooldown['swap']==0:
+            p1.rect, p2.rect = p2.rect, p1.rect
+            p1.cooldown['swap']=600
+
+    def freeze_activate(self,p1,p2):
+        if p1.distance(p2)<=200 and p1.cooldown['freeze']==0:
+            p1.speed/=2
+            p1.cooldown['freeze']=900
+    
+    def freeze_deactivate(self,p2):
         p2.speed*=2

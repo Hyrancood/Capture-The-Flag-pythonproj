03944a75ce6a8999e3b7a749acf17617f473dcd2
yenaii <andrejsuslov1441@gmail.com>
2024-12-11 20:33:17 +0300

    Player handler v1

--- player.py
+++ player.py
@@ -1,12 +1,16 @@
 import vector
 import pygame
 
+
+G_ACCELERATION = 0.2
+
+
 class Player():
     def __init__(self,chosen_color):
         self.color=chosen_color
         self.in_air=0
         self.g_acceleration=1
-        if self.color=='blue':
+        if self.color=='red':
             self.movement_buttons = {1073741903:'right', 1073741904:'left', 1073741906:'up'}
         else:
             self.movement_buttons = {100:'right', 97:'left', 119:'up'}
@@ -15,7 +19,7 @@ class Player():
         self.second_ability=None
         self.x=0
         self.y=0
-        self.speed=1
+        self.speed=3.3
         self.velocity=vector.Vector()
     def set_abilities(self,abilities):
         for i1 in range(len(abilities)):
@@ -32,25 +36,32 @@ class Player():
             return 'blue'
         else: return 'red'
     def update(self,**kwargs):
-        self.handle_events(kwargs['events'])
-        #self.handle_movement(**kwargs)
+        #self.handle_events(kwargs['events'])
+        self.handle_movement(**kwargs)
     def handle_movement(self,**kwargs):
         for event in kwargs['events']:
             if event.type == pygame.KEYDOWN:
-               if self.movement_buttons.get(event.key)=='right':
+                if self.movement_buttons.get(event.key)=='right':
                     self.velocity.x+=self.speed
                 if self.movement_buttons.get(event.key)=='left':
                     self.velocity.x-=self.speed
                 if self.movement_buttons.get(event.key)=='up':
+                    self.velocity.y-=self.speed
+            if event.type == pygame.KEYUP:
+                if self.movement_buttons.get(event.key)=='right':
+                    self.velocity.x-=self.speed
+                if self.movement_buttons.get(event.key)=='left':
+                    self.velocity.x+=self.speed
+                if self.movement_buttons.get(event.key)=='up':
                     self.velocity.y+=self.speed
         #проверка на нахождение в воздухе
-        #if in_air: self.in_air+=1
-        #else: self.in_air=0
-        self.velocity.y-=self.in_air*self.g_acceleration
+        #if not in_air:
+        #print(f'{self.color}: {self.rect}, {self.velocity.x}')
+        #self.velocity.y+=self.g_acceleration
         self.x+=self.velocity.x
         self.y+=self.velocity.y
-        self.velocity.x=0
-        self.velocity.y=0
+        self.rect.move_ip(self.velocity.x, self.velocity.y)
+        #print(f'{self.color}: x:{self.x} y: {self.y}, vel_x:{self.velocity.x} vel_y:{self.velocity.y}')
 
 
 
